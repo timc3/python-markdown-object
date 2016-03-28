baae44a2fcd17a3dd3d054e9002a64a086e34fdf
Tim Child <tim@timc3.com>
2016-03-28 12:14:30 +0200

    Create setup.py
    Adding a basic setup.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='python-markdown-object',
+    version='0.1',
+    install_requires=[
+        'Markdown',
+    ],
+    py_modules=['mdx_object'],
+)
